5ae989c36dbb64074949beb0377fd241ad79d00b
Chinmay Raj Nataraj <chinmayraj0527@gmail.com>
2025-02-22 13:41:47 +0530

    Updated Redirect

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,16 +1,15 @@
 import { Navigate } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
 
-
 const isAuthenticated = () => {
     const token = localStorage.getItem("token");
     if (!token) return false;
 
     try {
         const decoded = jwtDecode(token);
-    
+
         if (!decoded.exp || decoded.exp * 1000 < Date.now()) {
-            localStorage.removeItem("token"); 
+            localStorage.removeItem("token");
             return false;
         }
 
@@ -22,9 +21,8 @@ const isAuthenticated = () => {
     }
 };
 
-
 const PrivateRoute = ({ children }) => {
-    return isAuthenticated() ? children : <Navigate to="/" />;
+    return isAuthenticated() ? children : <Navigate to="/study-buddy-frontend/" replace />;
 };
 
 export default PrivateRoute;
